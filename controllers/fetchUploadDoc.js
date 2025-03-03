626bd160ee702d5ac605a9137e89f214f6fbaabe
Syed-webdevelpor <89412744+Syed-webdevelpor@users.noreply.github.com>
2025-03-03 22:49:51 +0500

    Update fetchUploadDoc.js

--- controllers/fetchUploadDoc.js
+++ controllers/fetchUploadDoc.js
@@ -7,6 +7,7 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
+  version:'latest'
 });
 
 // Environment variables
