16c9f83d105706d579f34dc0dd0fc2619abdcfe7
Syed-webdevelpor <89412744+Syed-webdevelpor@users.noreply.github.com>
2025-03-03 23:40:33 +0500

    Update fetchUploadDoc.js

--- controllers/fetchUploadDoc.js
+++ controllers/fetchUploadDoc.js
@@ -5,7 +5,7 @@ const DB = require("../dbConnection.js");
 
 // Configure AWS S3
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
   version:'latest'
