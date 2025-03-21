a987306db5dc816d2e71ecf24d8f4d8edb756e8b
Syed-webdevelpor <89412744+Syed-webdevelpor@users.noreply.github.com>
2025-03-21 21:29:38 +0500

    Update tradingAccount.js

--- controllers/tradingAccount.js
+++ controllers/tradingAccount.js
@@ -15,6 +15,12 @@ const fetchTradingAccountByUserID = async (id) => {
   return row;
 };
 
+const fetchaccountFinancialByUserID = async (id) => {
+  sql = "SELECT * FROM `account_financials` WHERE `userId`=?";
+  const [row] = await DB.execute(sql, [id]);
+  return row;
+}
+
 module.exports = {
 
   createAccount: async (req, res, next) => {
@@ -131,15 +137,23 @@ module.exports = {
       const data = verifyToken(req.headers.access_token);
       if (data && data.status) return res.status(data.status).json(data);
       const trading_accounts = await fetchTradingAccountByUserID(req.params.userId);
-      if (trading_accounts.length == 0) {
+      const account_financials = await fetchaccountFinancialByUserID(req.params.userId);
+      if (account_financials.length == 0) {
         return res.status(404).json({
           status: 404,
           message: "Trading accounts not found",
         });
       }
+      if (trading_accounts.length == 0) {
+        return res.status(404).json({
+          status: 404,
+          message: "account financials not found",
+        });
+      }
       res.json({
         status: 200,
         trading_accounts: trading_accounts,
+        account_financials: account_financials
       });
     } catch (err) {
       next(err);
