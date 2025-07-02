1eabf65ab97bc668efefc549cd79bfb9d11bdf57
Syed Junaid Sherazi <junaidsherazi535@gmail.com>
2025-07-02 19:46:44 +0500

    body size increase

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 const allowedOrigins = [
   'http://localhost:3000',
--- routers/orgInfoDoc.js
+++ routers/orgInfoDoc.js
@@ -5,7 +5,10 @@ const orgInfoDoc = require('../controllers/orgInfoDoc');
 
 const router = express.Router();
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB per file
+});
 
 router.post('/upload', upload.array('files'), orgInfoDoc.uploadFiles);
 router.get('/get/:userId', orgInfoDoc.getUserDocuments);
