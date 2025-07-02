daefc35e9c123e91a598db09e257f53aa02abb61
Syed Junaid Sherazi <junaidsherazi535@gmail.com>
2025-07-02 20:11:50 +0500

    Update s3Client.js

--- middlewares/s3Client.js
+++ middlewares/s3Client.js
@@ -2,11 +2,10 @@
 const AWS = require('aws-sdk');
 require('dotenv').config();
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
+  version:'latest'
 });
-
-const s3 = new AWS.S3();
 module.exports = s3;
